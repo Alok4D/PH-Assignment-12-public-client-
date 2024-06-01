d5c1809d6a790db1e91ac9d3b8fd99371dd00818
Alok4D <alokroy602701@gmail.com>
2024-06-01 23:50:46 +0600

    Building Management Project Setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='uppercase text-red-500 text-5xl'>Building Management System</h1>
+   
+    
+    </>
+  )
+}
+
+export default App
