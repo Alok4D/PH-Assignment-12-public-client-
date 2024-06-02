040c4cd5d4924a4dce1e08a8b65f02ead67feda1
Alok4D <alokroy602701@gmail.com>
2024-06-03 00:32:09 +0600

    add pgination

--- src/Pages/Apartment/Apartment.jsx
+++ src/Pages/Apartment/Apartment.jsx
@@ -1,8 +1,25 @@
 import { useEffect, useState } from "react";
 import ApartmentData from "./ApartmentData";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
+
 
 
 const Apartment = () => {
+    // pagination
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      };
+
     const [apartment, setApartment] = useState([]);
     console.log(apartment);
     useEffect( () => {
@@ -17,11 +34,21 @@ const Apartment = () => {
           Our apartments are available
           </h2>
         </div>
+       
+        <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
         <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-5 ">
             {
                 apartment.map(data => <ApartmentData key={data.Rent} data={data}></ApartmentData>)
             }
         </div>
+        </SwiperSlide>
+       
+      </Swiper>
         </div>
     );
 };
