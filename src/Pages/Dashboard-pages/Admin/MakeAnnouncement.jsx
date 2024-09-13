506c2c45ff89d009c1eb2da958ae9bc46cd8bba6
Alok4D <alokroy602701@gmail.com>
2024-09-13 11:28:03 +0600

    add make announcement section

--- src/Pages/Dashboard-pages/Admin/MakeAnnouncement.jsx
+++ src/Pages/Dashboard-pages/Admin/MakeAnnouncement.jsx
@@ -1,86 +1,72 @@
+import Swal from "sweetalert2";
+import UseAxiosSecure from "../../../hooks/UseAxiosSecure";
+import './AnnouncementForm.css';
 
-import { useState } from 'react';
-import './AnnouncementForm.css'; 
-import { useMutation } from '@tanstack/react-query';
-import UseAxiosSecure from '../../../hooks/UseAxiosSecure';
+const MakeAnnouncement = () => {
+  const axiosSecure = UseAxiosSecure();
 
-
-function AnnouncementForm() {
-    const axiosSecure = UseAxiosSecure();
-  // State to hold form data
-  const [formData, setFormData] = useState({
-    title: '',
-    description: ''
-  });
-  //
-  const { mutateAsync } = useMutation({
-    mutationFn: async announcementData => {
-        const { data } = await axiosSecure.post(`/announcement`, announcementData)
-        return data
-    },
-    onSuccess: () => {
-        console.log('announcement data save in db!');
-    }
-  })
-
-  // Handle input changes
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-  try{
-    const announcementData = {
-        title: '',
-        description: ''
-    }
-    console.log(announcementData)
-//  await mutateAsync(announcementData)
-  } catch (err) {
-    console.log(err);
-  }
-
-  // Handle form submission
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    console.log('Form submitted with data:', formData);
-   
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    console.log(title, description);
+    const newAnnouncement = {
+      title,
+      description,
+    };
+
+    axiosSecure.post("/announcement", newAnnouncement).then((res) => {
+      if (res.data.insertedId) {
+        console.log("admin added announcement!");
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "User Create Successfully!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        // navigate('/');
+        e.target.reset();
+      }
+    });
   };
 
   return (
-    <div className="announcement-form-container">
-      <h1>Create an Announcement</h1>
-      <form onSubmit={handleSubmit} className="announcement-form">
-        <div className="form-group">
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            className="form-control"
-            required
-          />
+    <div>
+      <div className="w-full flex justify-center items-center z-0 p-4 gap-10 flex-col lg:flex-row  shadow  mx-auto  dark:bg-gray-50 dark:text-gray-800">
+        <div className="w-full max-w-md p-4 rounded-md sm:p-8 shadow border dark:bg-gray-50">
+          <form onSubmit={handleLogin} className="space-y-8">
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <label htmlFor="email" className="block text-sm">
+                  Title :
+                </label>
+                <input
+                  type="title"
+                  name="title"
+                  id="title"
+                  // placeholder="leroy@jenkins.com"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="description">Description :</label>
+                <textarea
+                  name="description"
+                  className="form-control"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
+
+            </div>
+            <button type="submit" className="submit-button">Submit</button>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            className="form-control"
-            rows="4"
-            required
-          ></textarea>
-        </div>
-        <button type="submit" className="submit-button">Submit</button>
-      </form>
+      </div>
     </div>
   );
-}
+};
 
-export default AnnouncementForm;
+export default MakeAnnouncement;
--- src/Pages/Dashboard-pages/User-Dashboard/MyProfile.jsx
+++ src/Pages/Dashboard-pages/User-Dashboard/MyProfile.jsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 
 
+
 const MyProfile = () => {
     const {user} = useContext(AuthContext);
-    console.log(user);
+
+
+   
     return (
         <div className="flex flex-col items-center justify-center md:flex-row mt-6">
+
             <div className="group relative  sm:w-[350px]">
                 <img width={350} height={350} className="h-full w-full scale-105 transform rounded-lg bg-black/70" src={user?.photoURL} alt="card navigate ui" />
                 <span className="absolute -bottom-6 left-1/2 z-30 flex h-[40px] w-[40px] -translate-x-1/2 transform items-center  justify-center rounded-full bg-white bg-gradient-to-tr from-[#0d87f8]  to-[#70c4ff] duration-500 group-hover:rotate-180 group-hover:shadow-[0px_0px_30px_2px_#0d87f8]"><svg width={25} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g><g id="style=linear"><g id="add"><path id="vector" d="M11.998 5.84424L11.998 18.1604" stroke="#9EE6FD" strokeWidth="2" strokeLinecap="round"></path><path id="vector_2" d="M18.1561 12.002L5.83998 12.0019" stroke="#9EE6FD" strokeWidth="2" strokeLinecap="round"></path></g></g></g></svg></span>
@@ -35,7 +39,7 @@ const MyProfile = () => {
                 <div>
                     <button className="rounded-full border border-[#0d87f8] px-4 py-2 text-sm text-[#0d87f8] hover:bg-[#0d87f8] hover:text-white  duration-300 dark:hover:bg-transparent dark:hover:text-[#0d87f8] dark:hover:drop-shadow-[0px_0px_2px_#0d87f8]">SEND MESSAGE</button>
                 </div>
-            </div>
+            </div> 
         </div>  
      );
 };
